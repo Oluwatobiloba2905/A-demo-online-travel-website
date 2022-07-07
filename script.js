ef85fff4b602f54a68045a158df077148e35c107
Oluwatobiloba2905 <104198149+Oluwatobiloba2905@users.noreply.github.com>
2022-07-07 09:46:18 -1200

    A html and css travel website.

---
+++ script.js
@@ -0,0 +1,45 @@
+// const axios = input-group-text-btn("axios");
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://countries-cities.p.rapidapi.com/location/country/list',
+//   headers: {
+//     'X-RapidAPI-Key': '04e85d4b9cmsh5855dca0f256265p19062bjsn78e07fb9e9b3',
+//     'X-RapidAPI-Host': 'countries-cities.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+
+
+
+// const axios = require("axios");
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://countries-cities.p.rapidapi.com/location/country/list',
+//   headers: {
+//     'X-RapidAPI-Key': '04e85d4b9cmsh5855dca0f256265p19062bjsn78e07fb9e9b3',
+//     'X-RapidAPI-Host': 'countries-cities.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+
+
+
+// const search = window.getElementByClassname("input-group-text-btn")
+const show = document.getElementByClassName('login-btn');
+
+show.onmouseover = () => show.style.backgroundColor = 'yellow';
+// show.onmouseover = () => show.style.backgroundColor = 'yellow';
